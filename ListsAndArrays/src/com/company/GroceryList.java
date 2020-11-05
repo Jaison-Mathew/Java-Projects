a003d8aa6d32f6cfa9501fb8510f379d82c9379f
Jaison Mathew <mjaison29@gmail.com>
2020-11-05 16:17:06 -0500

    Practicing the ways of modifying array lists

---
+++ ListsAndArrays/src/com/company/GroceryList.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    //adds items in groceryList
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+
+    }
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    //prints items in groceryList
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your list");
+        for (int i = 0; i < groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem){
+
+        int postion = finditem(currentItem);
+        if (postion >= 0){
+            modifyGroceryItem(postion, newItem);
+        }
+    }
+
+    //modifies items in groceryList
+    private void modifyGroceryItem(int position, String groceryItem){
+        groceryList.set(position, groceryItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified");
+    }
+
+    public void removeGroceryItem(String item){
+        int postion = finditem(item);
+        if (postion >= 0){
+            removeGroceryItem(postion);
+        }
+    }
+
+    //removes items from groceryList
+    private void removeGroceryItem(int position){
+        groceryList.remove(position);
+    }
+
+    private int finditem(String searchitem){
+        return groceryList.indexOf(searchitem);
+    }
+
+    //checks if the item is in the array list
+    public boolean onFile(String searchItem){
+        int position = finditem(searchItem);
+        if (position >= 0){
+            return true;
+        }
+        return false;
+    }
+}
