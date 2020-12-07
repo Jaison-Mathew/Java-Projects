2d30a63ebe6e3c311d957a3cdd735f343661765c
Jaison Mathew <mjaison29@gmail.com>
2020-12-07 18:38:43 -0500

    Practicing the concept of polymorphism in classes

--- CarPolymorphism/src/com/company/Main.java
+++ CarPolymorphism/src/com/company/Main.java
@@ -1,14 +1,17 @@
 package com.company;
 
+//can also code classes in main class
 class Car{
     private String name;
     private int wheels;
     private int cylinders;
+    private boolean engine;
 
     public Car(String name, int cylinders){
         this.name = name;
         this.cylinders = cylinders;
         this.wheels = 4;
+        this.engine = true;
     }
 
     public String getName() {
@@ -25,72 +28,81 @@ class Car{
 
     //create method to start the engine
     public String startEngine(){
-        return "The engine has been turned on";
+        return "Car -> startEngine()";
     }
 
     //create method that accelerates the car
     public String accelerate(){
-        return "Car accelerated by 0mph";
+        return "Car -> accelerate()";
     }
 
     //create method that activates the brake
     public String brake(){
-        return "The brake has been activated";
+        return "Car -> brake()";
     }
 
 }
 
-class lambo extends Car{
-    public lambo() {
-        super("Lamborghini", 8);
+class Mitsubishi extends Car{
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
     }
 
     @Override
     public String startEngine() {
-        return "The engine started automatically";
+        return "Mitsubishi -> startEngine()";
     }
 
     @Override
     public String accelerate() {
-        return "Lambo has accelerated to 60mph";
+        return "Mitsubishi -> startEngine()";
     }
 
-    //No brake
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
 }
 
-class batmobile extends Car{
-    public batmobile() {
-        super("Batmobile", 16);
+class Holden extends Car{
+    public Holden(String name, int cylinders) {
+        super(name, cylinders);
     }
 
     @Override
     public String startEngine() {
-        return "The engine started using DNA scanner";
+        return "Holden -> startEngine()";
     }
 
     @Override
     public String accelerate() {
-        return "The Batmobile has accelerated to 120mph";
+        return "Holden -> accelerate()";
     }
 
     @Override
     public String brake() {
-        return "The emergency brake has been activated";
+        return "Holden -> brake()";
     }
 }
 
-class sedan extends Car{
-    public sedan() {
-        super("Sedan", 6);
+class Ford extends Car{
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
     }
 
-    //No engine start
-    //No accelerate
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
 
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
 
     @Override
     public String brake() {
-        return "Foot brake activated";
+        return "Ford -> brake()";
     }
 }
 
@@ -98,27 +110,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        batmobile batmobile = new batmobile();
-        System.out.println("The " + batmobile.getName() + " has " + batmobile.getCylinders() + " cylinders");
-
-        Car car = new Car("honda", 4);
-        System.out.println(car.getCylinders());
+        Car car = new Car("Base car", 8);
+        System.out.println(car.startEngine());
         System.out.println(car.accelerate());
-        System.out.println(car.getName());
         System.out.println(car.brake());
 
-        sedan sedan = new sedan();
-        System.out.println(sedan.getName());
-        System.out.println(sedan.getCylinders());
-        System.out.println(sedan.accelerate());
-        System.out.println(sedan.brake());
+        Mitsubishi mitsubishi = new Mitsubishi("Outlander VRX 4WD", 6);
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
 
-        lambo lambo = new lambo();
-        System.out.println(lambo.getName());
-        System.out.println(lambo.startEngine());
-        System.out.println(lambo.getCylinders());
-        System.out.println(lambo.accelerate());
-        System.out.println(lambo.brake());
+        Holden holden = new Holden("Holden Commodore", 6);
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
+        Ford ford = new Ford("Ford Falcon", 6);
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+        System.out.println(ford.getName());
 
     }
 
