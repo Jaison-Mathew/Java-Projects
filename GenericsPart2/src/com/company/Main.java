e8909f066e27c198097337d70713e88c2119d8fd
Jaison Mathew <mjaison29@gmail.com>
2021-01-10 22:25:14 -0500

    Practicing the concept of generics and comparables.

---
+++ GenericsPart2/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import javax.swing.text.TabExpander;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResults(fremantle, 1, 0);
+        hawthorn.matchResults(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResults(fremantle, 2, 1);
+        adelaideCrows.matchResults(adelaideCrows, 1, 1);
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+    }
+}
