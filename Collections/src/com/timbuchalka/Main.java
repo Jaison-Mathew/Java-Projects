8a37c7437141ce97d38f39a896bb96e56bfedfe1
Jaison Mathew <mjaison29@gmail.com>
2021-02-17 16:46:54 -0500

    Practicing the concept of comparable and comparator methods

--- Collections/src/com/timbuchalka/Main.java
+++ Collections/src/com/timbuchalka/Main.java
@@ -1,6 +1,7 @@
 package com.timbuchalka;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,8 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    Theatre theatre = new Theatre("Olympian", 8, 12);
-	    theatre.getSeats();
-	    
+/*	    theatre.getSeats();
+
 	    if (theatre.reserveSeat("H11")){
             System.out.println("Please pay");
         }else {
@@ -25,13 +26,38 @@ public class Main {
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
         printList(seatCopy);
 
+
         seatCopy.get(1).reserve();
-        if (theatre.reserveSeat("A02")){
-            System.out.println("Please pay for A02");
+ */
+        if (theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
         }else {
             System.out.println("Seat already reserved");
         }
 
+        if (theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
+        }else {
+            System.out.println("Seat already reserved");
+        }
+
+        if (theatre.reserveSeat("B13")){
+            System.out.println("Please pay for B13");
+        }else {
+            System.out.println("Seat already reserved");
+        }
+
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
+
+/*
         Collections.shuffle(seatCopy);              // prints collection list in random order
         // Collections.reverse(seatCopy);               // prints collection list in reverse order
         System.out.println("Printing seatCopy");
@@ -47,16 +73,18 @@ public class Main {
         sortList(seatCopy);
         System.out.println("Printing sorted copy");
         printList(seatCopy);
+*/
     }
 
     public static void printList(List<Theatre.Seat> list){
         for (Theatre.Seat seat : list){
-            System.out.println(" " + seat.getSeatNumber());
+            System.out.println(" " + seat.getSeatNumber() + " $" + seat.getPrice());
         }
         System.out.println();
         System.out.println("=================================================");
     }
 
+/*
     public static void sortList(List<? extends Theatre.Seat> list){
         for (int i = 0; i<list.size(); i++){
             for (int j = i+1; j<list.size(); j++){          // using bubble swap method but with nested for loop
@@ -66,4 +94,5 @@ public class Main {
             }
         }
     }
+*/
 }
