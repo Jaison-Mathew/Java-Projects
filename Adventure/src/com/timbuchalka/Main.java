c32bf57664b1569c84cebdb4576b6d3897ab6e81
Jaison Mathew <mjaison29@gmail.com>
2021-02-22 17:05:04 -0500

    Practicing the concept of exits for locations in maps

--- Adventure/src/com/timbuchalka/Main.java
+++ Adventure/src/com/timbuchalka/Main.java
@@ -19,18 +19,71 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+        // adding exits for location 1
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+//        locations.get(1).addExit("Q", 0);
+
+        // adding exits for location 2
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        // adding exits for location 3
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        // adding exits for location 4
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        // adding exits for location 5
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
         int loc = 1;
         // loop outputs location value depending on scanner value inputted
+
         while (true){
             System.out.println(locations.get(loc).getDescription());
             if (loc == 0){
                 break;
             }
 
-            loc = scanner.nextInt();
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()){
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if (exits.containsKey(direction)){
+                loc = exits.get(direction);
+            }else {
+                System.out.println("You cannot go in that direction");
+            }
+
             if (!locations.containsKey(loc)){
                 System.out.println("You cannot go that direction");
             }
+
+        }
+
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for (String i : road) {
+            System.out.println(i);
+        }
+
+        System.out.println("==========================================");
+
+        String[] building = "You are inside a building, a well house for a small spring".split(", ");
+        for (String i : building) {
+            System.out.println(i);
         }
     }
 }
