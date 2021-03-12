04909a2087124cb1a27312ce97240dde8c42072b
Jaison Mathew <mjaison29@gmail.com>
2021-03-12 12:30:10 -0500

    Understanding potential issues overriding equals method and sub-classing

---
+++ SetAndHashSet/src/com/timbuchalka/Challenge.java
@@ -0,0 +1,11 @@
+package com.timbuchalka;
+
+public class Challenge {
+
+/*
+    Overriding equals() method in the HeavenlyBody class was unnecessary because the HeavenlyBody class is declared
+    final, so it cannot be subclassed. The Java String class is also final, which is why it can safely use the
+    instanceof method without having to worry about comparisons with a subclass.
+*/
+
+}
