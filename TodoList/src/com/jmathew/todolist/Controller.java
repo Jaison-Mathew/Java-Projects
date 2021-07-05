4572806656256c76e1efdcf87658e256a32bf838
Jaison Mathew <mjaison29@gmail.com>
2021-07-05 12:06:54 -0400

    Practicing the concept of KeyEvents and Toolbars

--- TodoList/src/com/jmathew/todolist/Controller.java
+++ TodoList/src/com/jmathew/todolist/Controller.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -154,6 +156,17 @@ public class Controller {
             System.out.println("OK pressed");
         }else{
             System.out.println("Cancel pressed");
+        }
+    }
+
+    // deletes selected item
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent){
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null){
+            if (keyEvent.getCode().equals(KeyCode.DELETE)){
+                deleteItem(selectedItem);
+            }
         }
     }
 
