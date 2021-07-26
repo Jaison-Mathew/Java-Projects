04428457c03819ac7430c3b8e300d082e3e5fc7a
Jaison Mathew <mjaison29@gmail.com>
2021-07-26 13:19:35 -0400

    Practicing the concept of changing nodes and adding choosers

--- JavaFXApplication/src/sample/Controller.java
+++ JavaFXApplication/src/sample/Controller.java
@@ -1,4 +1,53 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
+    public void initialize(){
+//        label.setScaleX(2.0);
+//        label.setScaleY(2.0);
+        button4.setEffect(new DropShadow());
+    }
+
+    @FXML
+    public void handleMouseEnter(){             //label grows when mouse is hovered over it
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){              //label returns to normal size when mouse is not hovered over it
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
+
+    @FXML
+    public void handleClick(){          //selects option to choose file or directory
+        //FileChooser chooser = new FileChooser();
+        DirectoryChooser chooser = new DirectoryChooser();
+        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        if (file != null){
+            System.out.println(file.getPath());
+        }else {
+            System.out.println("Chooser was cancelled");
+        }
+    }
 }
