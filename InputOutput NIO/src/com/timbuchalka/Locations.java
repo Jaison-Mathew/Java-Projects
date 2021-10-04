a54c9dbeabafd93fcb830379e14669c00279af45
Jaison Mathew <mjaison29@gmail.com>
2021-10-04 11:58:38 -0400

    Practicing writing objects with Java NIO

--- InputOutput NIO/src/com/timbuchalka/Locations.java
+++ InputOutput NIO/src/com/timbuchalka/Locations.java
@@ -14,6 +14,15 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
+
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+        try(ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))) {
+            for (Location location : locations.values()){
+                locFile.writeObject(location);
+            }
+        }
+
+/*
         Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
         Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
         try (BufferedWriter locFile = Files.newBufferedWriter(locPath);
@@ -32,9 +41,31 @@ public class Locations implements Map<Integer, Location> {
         }catch (IOException e){
             System.out.println("IOException: " + e.getMessage());
         }
+*/
     }
 
     static {
+
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(locPath)))){
+            boolean eof = false;
+            while (!eof){
+                try {
+                    Location location = (Location) locFile.readObject();
+                    locations.put(location.getLocationID(), location);
+                }catch (EOFException e){
+                    eof = true;
+                }
+            }
+        }catch (InvalidClassException e){
+            System.out.println("InvalidClassException " + e.getMessage());
+        }catch (IOException e){
+            System.out.println("IOException " + e.getMessage());
+        }catch (ClassNotFoundException e){
+            System.out.println("ClassNotFoundException " + e.getMessage());
+        }
+
+/*
         Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
         Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
 
@@ -50,6 +81,22 @@ public class Locations implements Map<Integer, Location> {
         }catch (IOException e){
             e.printStackTrace();
         }
+
+        try(BufferedReader dirFile = Files.newBufferedReader(dirPath)){
+            String input;
+            while ((input = dirFile.readLine()) != null){
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+*/
     }
 
     @Override
