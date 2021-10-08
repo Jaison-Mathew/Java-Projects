5a0b6de21427a8320dd401b06530e632fb1b7ddf
Jaison Mathew <mjaison29@gmail.com>
2021-10-08 12:23:47 -0400

    Practicing how to read files with Java NIO

--- JavaNio/src/com/timbuchalka/Main.java
+++ JavaNio/src/com/timbuchalka/Main.java
@@ -3,6 +3,7 @@ package com.timbuchalka;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -33,8 +34,55 @@ public class Main {
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
 
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] = 'a';               //changes first element of byte array
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if (buffer.hasArray()){
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+
+            //Absolute read
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));            //starts read at index position
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+
+
+
+/*          Relative read
+
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            intBuffer.flip();
+            System.out.println(intBuffer.getInt());
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            intBuffer.flip();
+            System.out.println(intBuffer.getInt());
+            channel.close();
+            ra.close();
+*/
+
+            //System.out.println("outputBytes = " + new String(outputBytes));
 
 /*
+            // reads files using NIO
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            byte[] b = new byte[outputBytes.length];                    // defines new byte array that contains data being read
+            ra.read(b);                                                // passes what is read into the byte array
+            System.out.println(new String(b));
+
+            //reads the integers from buffers
+            long int1 = ra.readInt();
+            long int2 = ra.readInt();
+            System.out.println(int1);
+            System.out.println(int2);
+
             FileInputStream file = new FileInputStream("data.txt");         //create file input stream
             FileChannel channel = file.getChannel();
 
