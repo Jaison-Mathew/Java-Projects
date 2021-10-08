bea74144d7b9d62a98ae769658e0cdcdae5bff0c
Jaison Mathew <mjaison29@gmail.com>
2021-10-08 11:54:26 -0400

    Practicing writing binary files with Java NIO

--- JavaNio/src/com/timbuchalka/Main.java
+++ JavaNio/src/com/timbuchalka/Main.java
@@ -1,7 +1,9 @@
 package com.timbuchalka;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -12,17 +14,37 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()){         //output stream will be closed when execution is finished because using try with resources
+            byte[] outputBytes = "Hello World!".getBytes();         //creates byte array
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);           //wraps byte array into the buffer
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);      //number of bytes in an integer
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+
 /*
             FileInputStream file = new FileInputStream("data.txt");         //create file input stream
             FileChannel channel = file.getChannel();
-*/
+
             Path dataPath = FileSystems.getDefault().getPath("data.txt");
             Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);         //writes data to file
             List<String> lines = Files.readAllLines(dataPath);          //reads entire into memory,and returns list of string objects
             for (String line : lines){
                 System.out.println(line);
             }
+*/
         }catch (IOException e){
             e.printStackTrace();
         }
