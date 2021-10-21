8f582e9870edac015ee6a9eed034f6e46fc59442
Jaison Mathew <mjaison29@gmail.com>
2021-10-21 11:56:16 -0400

    Practicing using different file attributes.

--- Paths/src/com/timbuchalka/Main.java
+++ Paths/src/com/timbuchalka/Main.java
@@ -1,8 +1,10 @@
 package com.timbuchalka;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
@@ -28,10 +30,35 @@ public class Main {
             Path fileToRename = FileSystems.getDefault().getPath("Examples", "file1.txt");
             Path fileName = FileSystems.getDefault().getPath("Examples", "file2.txt");
             Files.move(fileToRename, fileName);
-*/
+
             //Deleting file
             Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
             Files.deleteIfExists(fileToDelete);
+
+            //Creating file
+            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+            Files.createFile(fileToCreate);
+
+            //Creating directory
+            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+            Files.createDirectory(dirToCreate);
+*/
+            //Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2\\Dir\\Dir3\\Dir4\\Dir5\\Dir6");
+            Path dirToCreate = FileSystems.getDefault().getPath("Examples\\Dir2\\Dir\\Dir3\\Dir4\\Dir5\\Dir6\\Dir7");
+            Files.createDirectories(dirToCreate);
+
+            //Finding the size of a file
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1\\file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));               //Displays when file was last modified
+
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attrs.size());
+            System.out.println("Last modified = " + attrs.lastModifiedTime());
+            System.out.println("Created = " + attrs.creationTime());
+            System.out.println("Is directory = " + attrs.isDirectory());
+            System.out.println("Is regular file = " + attrs.isRegularFile());
 
         }catch (IOException e){
             System.out.println(e.getMessage());
