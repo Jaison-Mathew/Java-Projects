5cb3db65b6fa7fff2e60ae18070efebffc0eb597
Jaison Mathew <mjaison29@gmail.com>
2021-10-23 12:03:31 -0400

    Practicing coding on how to read existing directory contents.

---
+++ Directories/src/com/timbuchalka/Main.java
@@ -0,0 +1,30 @@
+package com.timbuchalka;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>(){
+            public boolean accept(Path path) throws IOException{
+                return (Files.isRegularFile(path));
+            }
+        };
+*/
+        //Same directory stream function but with Lambda expression
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
+
+        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        //reads existing contents in path directory
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)){
+            for (Path file : contents){
+                System.out.println(file.getFileName());
+            }
+        }catch (IOException | DirectoryIteratorException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
