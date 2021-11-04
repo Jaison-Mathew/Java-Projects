ed5032e82074c074d0a835cc5855f401acbd2293
Jaison Mathew <mjaison29@gmail.com>
2021-11-04 12:43:51 -0400

    Practicing using Join and Interrupt methods.

--- Threads/src/com/timbuchalka/Main.java
+++ Threads/src/com/timbuchalka/Main.java
@@ -21,10 +21,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();           //calling join method of the thread that we want to join the current thread to.
+                    System.out.println(ANSI_RED + "AnotherThread terminated or timed out, so I'm running again.");
+                }catch (InterruptedException e){
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                }
             }
         });
 
         myRunnableThread.start();
+        //anotherThread.interrupt();
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
 
