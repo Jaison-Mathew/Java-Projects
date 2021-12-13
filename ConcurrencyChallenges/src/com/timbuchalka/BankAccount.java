5165d6984825da562ae3017fc26e0afeadb07dbd
Jaison Mathew <mjaison29@gmail.com>
2021-12-13 14:46:43 -0500

    Using ReentrantLock class, instead of a synchronized keyword, to make the BankAccount class threadsafe.

--- ConcurrencyChallenges/src/com/timbuchalka/BankAccount.java
+++ ConcurrencyChallenges/src/com/timbuchalka/BankAccount.java
@@ -1,20 +1,36 @@
 package com.timbuchalka;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
 
+    private Lock lock;             //only one lock object is needed because both threads need to compete for the same lock.
+
     public BankAccount(String accountNumber, double initialBalance) {
         this.balance = initialBalance;
         this.accountNumber = accountNumber;
+        this.lock = new ReentrantLock();        //Using ReentrantLock class, instead of synchronized keyword, to make the BankAccount class threadsafe.
     }
 
-    public synchronized void deposit(double amount){
-        balance += amount;
+    public void deposit(double amount){
+        lock.lock();
+        try {           //the critical section of code is in the try/finally block to ensure that the lock will be released.
+            balance += amount;
+        }finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void withdraw(double amount){
-        balance -= amount;
+    public void withdraw(double amount){
+        lock.lock();
+        try{
+            balance -= amount;
+        }finally {
+            lock.unlock();
+        }
     }
 
     public String getAccountNumber(){
