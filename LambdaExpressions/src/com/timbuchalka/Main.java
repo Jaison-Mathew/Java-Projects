1e87cec447584dab30bf05d4d2afe53dd85a8d47
Jaison Mathew <mjaison29@gmail.com>
2022-01-16 10:34:56 -0500

    Practicing using streams and filters on Lambda expressions.

--- LambdaExpressions/src/com/timbuchalka/Main.java
+++ LambdaExpressions/src/com/timbuchalka/Main.java
@@ -1,7 +1,11 @@
 package com.timbuchalka;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -43,6 +47,42 @@ public class Main {
         Supplier<String> iLoveJava = () -> { return "I love Java"; };
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
+
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+//        topNames2015.forEach(name ->
+//                firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1)));
+//        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+//        firstUpperCaseList.forEach(s -> System.out.println(s));
+//        firstUpperCaseList.sort(String::compareTo);
+//        firstUpperCaseList.forEach(System.out::println);
+
+        topNames2015.stream().map(name -> name.substring(0, 1).toUpperCase() + name.substring(1)).sorted(String::compareTo).forEach(System.out::println);
+
+        long namesBeginningWithA = topNames2015.stream().map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(name -> name.startsWith("A"))
+                .count();
+        System.out.println("Number of names beginning with A is: " + namesBeginningWithA);
+        System.out.println("-------------------------------------------");
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
 
     }
 
