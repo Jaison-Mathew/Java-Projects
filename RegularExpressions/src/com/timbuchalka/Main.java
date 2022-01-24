4dbd994c8ab9b804d817b137978d0720c802e42e
Jaison Mathew <mjaison29@gmail.com>
2022-01-24 13:01:46 -0500

    Practicing using And, Or, & Not on Regular expressions.

--- RegularExpressions/src/com/timbuchalka/Main.java
+++ RegularExpressions/src/com/timbuchalka/Main.java
@@ -98,6 +98,47 @@ public class Main {
         while (h2TextMatcher.find()){
             System.out.println("Occurrence: " + h2TextMatcher.group(2));
         }
+        // "abc" = "a" and "b" and "c"
+        // [Hh]arry
+        System.out.println("harry".replaceAll("[H|h]arry", "Larry"));
+        System.out.println("Harry".replaceAll("[H|h]arry", "Larry"));
+
+        // [^abc]
+        String tvTest = "tstvtkt";
+        //String tNotVRegExp = "t[^v]";
+        String tNotVRegExp = "t(?!v)";
+        Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
+        Matcher tNotVMatcher = tNotVPattern.matcher(tvTest);
+
+        count = 0;
+        while (tNotVMatcher.find()){
+            count++;
+            System.out.println("Occurrence " + count + " : " + tNotVMatcher.start() + " to " + tNotVMatcher.end());
+        }
+        // t(?=v)
+        // ^([\(]{1}[0-9]{3}[\)]{1}[ ]{1}[0-9]{3}[\-]{1}[0-9]{4})$             Sample US phone number (800)123-4567
+        String phone1 = "1234567890";       //Shouldn't match
+        String phone2 = "(123) 456-7890";       //Should match
+        String phone3 = "123 456-7890";         //Shouldn't match
+        String phone4 = "(123)456-7890";        //Shouldn't match
+
+        System.out.println("phone1 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone2 = " + phone2.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone3 = " + phone3.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone4 = " + phone4.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+
+        System.out.println("-----------------------------------------");
+
+        // ^4[0-9]{12}([0-9]{3})?$
+        String visa1 = "4444444444444";     //Shouldn't match
+        String visa2 = "5444444444444";     //Shouldn't match
+        String visa3 = "4444444444444444";      //Should match
+        String visa4 = "4444";              //Shouldn't match
+
+        System.out.println("visa1 = " + visa1.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa2 = " + visa2.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa3 = " + visa3.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa4 = " + visa4.matches("^4[0-9]{12}([0-9]{3})?$"));
 
 
     }
