1a7d6ca58aa3d26db17e953ed497c45df7987341
Jaison Mathew <mjaison29@gmail.com>
2022-04-06 10:55:17 -0400

    Practicing using INSERT, UPDATE, and DELETE with JDBC.

--- TestDB/src/com/timbuchalka/Main.java
+++ TestDB/src/com/timbuchalka/Main.java
@@ -1,17 +1,28 @@
 package com.timbuchalka;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    try{
             Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjava.db");
+            //conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE Table contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("CREATE Table IF NOT EXISTS contacts " + " (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Tim', 6545678, 'tim@email.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Jane', 4829484, 'jane@somewhere.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Fido', 9038, 'dog@email.com')");
+//            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Tim'");
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()){
+                System.out.println(results.getString("name") + " " + results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
+
+            results.close();
 
             //closing database resources
             statement.close();
