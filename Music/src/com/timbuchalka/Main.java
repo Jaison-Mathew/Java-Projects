1ff5d4ce90fc6338b5c190cc5a001a00f5e42679
Jaison Mathew <mjaison29@gmail.com>
2022-04-30 19:40:45 -0400

    Created method that queries the artists for a song.

--- Music/src/com/timbuchalka/Main.java
+++ Music/src/com/timbuchalka/Main.java
@@ -25,7 +25,7 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
-        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
 
         for(String album : albumsForArtist){
             System.out.println(album);
--- Music/src/com/timbuchalka/model/Datasource.java
+++ Music/src/com/timbuchalka/model/Datasource.java
@@ -42,6 +42,15 @@ public class Datasource {
 
     public static final String QUERY_ALBUMS_BY_ARTIST_SORT = " ORDER BY " + TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " COLLATE NOCASE ";
 
+    public static final String QUERY_ARTIST_FOR_SONG_START = "SELECT " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+            TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + ", " + TABLE_SONGS + "." + COLUMN_SONG_TRACK + " FROM " + TABLE_SONGS +
+            " INNER JOIN " + TABLE_ALBUMS + " ON " + TABLE_SONGS + "." + COLUMN_SONG_ALBUM + " = " + TABLE_ALBUMS + "." +
+            COLUMN_ALBUM_ID + " INNER JOIN " + TABLE_ARTISTS + " ON " + TABLE_ALBUMS + "." + COLUMN_ALBUM_ARTIST + " = " +
+            TABLE_ARTISTS + "." + COLUMN_ARTIST_ID + " WHERE " + TABLE_SONGS + "." + COLUMN_SONG_TITLE + " = \"";
+
+    public static final String QUERY_ARTIST_FOR_SONG_SORT = "ORDER BY " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+            TABLE_ARTISTS + "." + COLUMN_ALBUM_NAME + " COLLATE NOCASE ";
+
     private Connection conn;
 
     //returns true if connection is truly established, and returns false if otherwise.
