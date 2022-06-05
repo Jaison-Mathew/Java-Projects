40ff951766a12bd9de605fa590cc5d263438839c
Jaison Mathew <mjaison29@gmail.com>
2022-06-05 08:34:50 -0400

    Practicing Keywords and Naming Conventions.

---
+++ KeywordsAndExpressions/src/com/timbuchalka/Main.java
@@ -0,0 +1,11 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double int2 = 50.00d;
+        double milesDriven = 1000d;
+        int customers = 50;
+        char lastKey = 'd';
+    }
+}
