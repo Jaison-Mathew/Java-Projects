d517ab6616c64ccd779ff1ff35c78c8b30a773f6
Jaison Mathew <mjaison29@gmail.com>
2022-06-06 08:46:26 -0400

    Practicing Declaration and Expression Statements.

---
+++ Statements/src/com/timbuchalka/Main.java
@@ -0,0 +1,15 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int finalScore = 50;
+        int playersHighScore;
+
+        int calculatedScore = finalScore = 60;
+        System.out.println("Calculated Score = " + calculatedScore);
+
+        finalScore = 70;
+    }
+}
