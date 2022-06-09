14e054a5cdc51da54a4ba540fd5827729de32bf7
Jaison Mathew <mjaison29@gmail.com>
2022-06-09 19:43:13 -0400

    Practicing Code Blocks and Indentation.

---
+++ Indentation/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int salary = 60;
+        if (salary > 50){
+            System.out.println("You are rich!");
+        }
+    }
+}
