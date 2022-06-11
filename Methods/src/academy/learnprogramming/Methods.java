107eb760cdcaf2563fed6ab69f6b3caa5a29bcdd
Jaison Mathew <mjaison29@gmail.com>
2022-06-11 19:02:34 -0400

    Practicing executing Methods.

--- Methods/src/academy/learnprogramming/Methods.java
+++ Methods/src/academy/learnprogramming/Methods.java
@@ -8,11 +8,7 @@ public class Methods {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(isGameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore();
 
         score = 10000;
         levelCompleted = 8;
